ad00b4bc8839d3289c33faef441d8aa6156d7a70
tlabna <tlabna@users.noreply.github.com>
2018-10-01 13:23:34 -0400

    feat: add GraphQLError class for catching errors on CreateVote mutation
    - This didn't need to change and really just for demonstration purposes
    - Errors can be caught for graphQL using plain Python exceptions
    - However, graphQL does supply a GraphQLError class for error handling
      at the application level
    
    - Tested and both methods of catching execptions return the same result

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -5,6 +5,8 @@ from .models import Link, Vote
 
 from users.schema import UserType
 
+from graphql import GraphQLError
+
 
 class LinkType(DjangoObjectType):
     class Meta:
@@ -66,7 +68,7 @@ class CreateVote(graphene.Mutation):
     def mutate(self, info, link_id):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('You must be logged in to vote!')
+            raise GraphQLError('You must be logged in to vote!')
 
         link = Link.objects.filter(id=link_id).first()
         if not link:
