84079d4a6e3036a2df2fdeeb4b5e896a332855fc
tlabna <tlabna@users.noreply.github.com>
2018-10-01 12:23:54 -0400

    feat: add users to links
    - links/models.py:
      - add posted_by field to Link model
        - Foreign key field that links User model to Link
        - User must be authenticated to attach to a link
    
    - links/schema.py:
      - update CreateLink mutation to return User in the new created field
    
    - add migration of updated Link model

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -3,6 +3,8 @@ from graphene_django import DjangoObjectType
 
 from .models import Link
 
+from users.schema import UserType
+
 
 class LinkType(DjangoObjectType):
     class Meta:
@@ -21,6 +23,7 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     # Defines the data you can send to the server
     class Arguments:
@@ -31,13 +34,16 @@ class CreateLink(graphene.Mutation):
     # in this case the url and description.
     # After, the server returns the CreateLink class with the data just created.
     def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+        user = info.context.user or None
+
+        link = Link(url=url, description=description, posted_by=user)
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.description,
+            posted_by=link.posted_by,
         )
 
 
